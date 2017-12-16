26afd68f6929fabd0180f0f2e9ff410b19f1abe7
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-16 18:03:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var recipes = {
+  prop: 1
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
+}
+
+updateObjectWithKeyAndValue(recipes, prop2, 2);
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+destructivelyUpdateObjectWithKeyAndValue(recipes, prop2, 2);
+
+function deleteFromObjectByKey(object, key) {
+  var copy = Objects.assign({}, object);
+  delete copy[key];
+  return copy;
+}
+
